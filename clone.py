80fa3aa4f6276ca50459ab92ee81dc2bee4f73fb
Srikar Hanumanula <geetsrikar@gmail.com>
2021-04-05 11:05:53 -0400

    Voice Cloning Code Cleanup

--- clone.py
+++ clone.py
@@ -1,29 +1,32 @@
 import subprocess
 class clone:
     isRunning = False
+    proc = None
 
     def stop(self):
         self.isRunning = False
+        self.proc.kill()
 
     
     def clone(self, screen):
         self.isRunning = True
         n = screen.prevSlider
-        proc = self.update(n)
+        self.proc = self.update(n)
         while self.isRunning:
             if n != screen.prevSlider:
-                proc.kill()
+                self.proc.kill()
                 n = screen.prevSlider
-                self.update(n)
+                self.proc = self.update(n)
 
     def update(self, n):
-        
         value = int(n) * 100
         command = 'play -q -V0 "|rec -q -V0 -n -d -R riaa bend pitch {}"'.format(value)
         print("Voice Deformation Value: " + str(value))
         print("Voice Deformation Command: " + str(command))
-        proc = subprocess.Popen(command)
+        proc = subprocess.Popen(command, shell=True)
         return proc
+    
+
 
             
        
